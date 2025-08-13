a7d654cb989f0a94a67a826ca3487b9935f8a1c9
Patchara <sombrousnorn@gmail.com>
2025-08-13 15:59:20 +0700

    refactor:add state and onClick GreetingMessage

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import "./App.css";
-
+import { useState } from "react";
 function App() {
+  const [text, setText] = useState('Greeting Message')
+  const greetingMessage = (text) => {
+    setText(text)
+  }
   return (
     <div className="App">
-      <div className="greeting-container">Greeting Message</div>
+      <div className="greeting-container">{text}</div>
       <div className="buttons">
-        <button>สวัสดี!</button>
-        <button>Hi!</button>
-        <button>你好!</button>
+        <button onClick={() => greetingMessage("สวัสดี!")}>สวัสดี!</button>
+        <button onClick={() => greetingMessage("Hi!")}>Hi!</button>
+        <button onClick={() => greetingMessage("你好!")}>你好!</button>
       </div>
     </div>
   );
